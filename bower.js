3154a202cb1373755dc8ff950f3d95dd9f2b74d8
Jared Hardy <jared@jaredhardy.com>
2013-06-09 14:17:32 -0700

    add bower

---
+++ bower.js
@@ -0,0 +1,22 @@
+{
+  "name": "minimal-devices",
+  "version": "0.5.0",
+  "author": "Jared Hardy",
+  "description": "Minimal HTML and CSS devices",
+  "main": [
+    "css/imac.css",
+    "css/iphone.css",
+    "css/ipad.css",
+    "css/macbook.css",
+    "templates/device_markup/imac.html",
+    "templates/device_markup/ipad.html",
+    "templates/device_markup/iphone.html",
+    "templates/device_markup/macbook.html"
+  ], 
+  "ignore": [
+    "css-output/",
+    "lib/",
+    "pkg/",
+    "stylesheets/"
+  ]
+}
